31d1a0a68d8caa9c403dbebb52fe0550acbf40a7
Steve Faulkner <southpolesteve@gmail.com>
2011-12-25 16:03:03 -0600

    Activities index working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Challenge::Application.routes.draw do
-  get "activities/index"
 
   get "users/dashboard"
 
   devise_for :users
 
   root :to => "users#dashboard"
+
+  resources :activities, :only => [:index]
+
 end
