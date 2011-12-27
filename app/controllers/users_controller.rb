a5af22dde45375ffc817b937d95ea29bf70f790b
Steve Faulkner <southpolesteve@gmail.com>
2011-12-27 17:49:43 -0600

    Handle when user isn't logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
 
   def dashboard
-    @leaderboard_users = User.all.sort_by(&:total_points)
-    @recent_events = current_user.events.order('created_at DESC').limit(5)
-    @new_event = Event.new(:user => current_user)
+    if current_user
+      @leaderboard_users = User.all.sort_by(&:total_points)
+      @recent_events = current_user.events.order('created_at DESC').limit(5)
+      @new_event = Event.new(:user => current_user)
+    else
+      render "not_signed_in"
+    end
   end
 
 end
