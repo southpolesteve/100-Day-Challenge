da5456abe43da466d5c47d0a174dffdc329cdeb3
Steve Faulkner <southpolesteve@gmail.com>
2011-12-27 17:59:36 -0600

    Mispelled validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :activity
   belongs_to :user
 
-  validate_presence_of :user, :activity, :count
+  validates_presence_of :user, :activity, :count
 
 end
