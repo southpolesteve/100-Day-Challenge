7f1c95944d63828f5fb7ff0acfc06b19d3b9d790
Steve Faulkner <southpolesteve@gmail.com>
2011-12-27 17:51:13 -0600

    Added first and last name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111226013435) do
+ActiveRecord::Schema.define(:version => 20111227235041) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(:version => 20111226013435) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
