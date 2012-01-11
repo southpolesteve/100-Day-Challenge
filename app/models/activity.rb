cbd1c33e550e597240fc6b8014564777d764de61
Steve Faulkner <southpolesteve@gmail.com>
2012-01-10 23:38:59 -0600

    Fix calculation for alcohol

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,18 +4,22 @@ class Activity < ActiveRecord::Base
 
   def score_for_user_and_date(user,date)
     gross_score = events.where(:user_id => user.id, :completed_date => date).map{ |event| event.count*self.value }.sum
-    hundreds = gross_score/100
-    adjusted_score = 0
-    (hundreds+1).times do
-      if gross_score > 100
-        adjusted_score = adjusted_score + 100
-        gross_score = (gross_score - 100)/2
-      else
-        adjusted_score = adjusted_score + gross_score
-        gross_score = 0
+    unless name == "alcohol"
+      hundreds = gross_score/100
+      adjusted_score = 0
+      (hundreds+1).times do
+        if gross_score > 100
+          adjusted_score = adjusted_score + 100
+          gross_score = (gross_score - 100)/2
+        else
+          adjusted_score = adjusted_score + gross_score
+          gross_score = 0
+        end
       end
+      adjusted_score
+    else
+      gross_score
     end
-    adjusted_score
   end
 
 end
